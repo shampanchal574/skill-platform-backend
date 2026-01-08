2c2e52351b3f2e1b265df83b99fbbc8c46bf8b6c
Sham panchal <206090021+shampanchal574@users.noreply.github.com>
2026-01-08 15:30:37 +0530

    Update DATABASE_URL to use environment variable

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.environ.get("postgresql://skill_platform_user:O7eLickUoKyk8i9iWQAPCIQWWceUIDjZ@dpg-d5fnjb6r433s73b4cv20-a/skill_platform")
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
